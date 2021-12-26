37b3d61e58b79b41e957a149c9edd1febbf0a879
unknown <danyasven@gmail.com>
2021-12-26 23:46:00 +0200

    fdfdfdg

--- crm/settings.py
+++ crm/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-tp194)eu&%==!m
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fast-ocean-79662.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
