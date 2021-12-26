d904688c8bddd24f6b83681b6fc414bf998b5ece
unknown <danyasven@gmail.com>
2021-12-26 23:50:07 +0200

    'r'

--- crm/settings.py
+++ crm/settings.py
@@ -151,3 +151,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+import django_heroku
+django_heroku.settings(locals())
